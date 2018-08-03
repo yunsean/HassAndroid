4a619ae0dce83d2ff2c15e3b3d8d3ed4e0f4b5fc
Dylan <yunsean@163.com>
2018-08-03 17:29:47 +0800

    no message

---
+++ app/src/main/java/com/yunsean/ihass/Application.kt
@@ -0,0 +1,11 @@
+package com.yunsean.ihass
+
+import cn.com.thinkwatch.ihass2.HassApplication
+import com.tencent.bugly.Bugly
+
+class Application: HassApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        Bugly.init(getApplicationContext(), "3217dc351d", true)
+    }
+}
